2e667b99dea64356f0ff9357d5c648018bd34782
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-24 09:06:40 -0700

    Tab updates

--- compose.sh
+++ compose.sh
@@ -7,43 +7,43 @@
 # - Make sure you run this from the root of the top-level repo
 
 function printInfo {
-  # print blue
-  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+  	# print blue
+  	echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  # print yellow
-  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+  	# print yellow
+  	echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
 }
 
 function printError {
-  # print red
-  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+  	# print red
+  	echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
 }
 
 case $1 in
-  "down")
-    printWarning "Stopping the AgRobot container..."
-    docker compose -f docker/docker-compose.yaml down
-    ;;
-  *)
-    printInfo "Loading the AgRobot container..."
-    docker compose -f docker/docker-compose.yaml up -d
+	"down")
+		printWarning "Stopping the AgRobot container..."
+		docker compose -f docker/docker-compose.yaml down
+		;;
+	*)
+		printInfo "Loading the AgRobot container..."
+		docker compose -f docker/docker-compose.yaml up -d
 
-    # Check if a 'agrobot' tmux session already exists
-    if [ "$(docker exec -it agrobot tmux list-sessions | grep agrobot)" == "" ]; then
+		# Check if a 'agrobot' tmux session already exists
+		if [ "$(docker exec -it agrobot tmux list-sessions | grep agrobot)" == "" ]; then
 
-      # If not, create a new 'agrobot' tmux session
-      printWarning "Creating a new tmux session..."
-      docker exec -it agrobot tmux new-session -d -s agrobot
-      docker exec -it agrobot tmux select-pane -t 0 -T agrobot
-      docker exec -it agrobot tmux send-keys "clear && cat ~/config/introduction.txt" Enter
+			# If not, create a new 'agrobot' tmux session
+			printWarning "Creating a new tmux session..."
+			docker exec -it agrobot tmux new-session -d -s agrobot
+			docker exec -it agrobot tmux select-pane -t 0 -T agrobot
+			docker exec -it agrobot tmux send-keys "clear && cat ~/config/introduction.txt" Enter
 
-      # Full color and mouse options
-      docker exec -it agrobot tmux set-option -g default-terminal "screen-256color"
-      docker exec -it agrobot tmux set -g mouse on
-    fi
-    # Attach to the 'agrobot' tmux session
-    docker exec -it agrobot tmux attach -t agrobot
-    ;;
+			# Full color and mouse options
+			docker exec -it agrobot tmux set-option -g default-terminal "screen-256color"
+			docker exec -it agrobot tmux set -g mouse on
+		fi
+		# Attach to the 'agrobot' tmux session
+		docker exec -it agrobot tmux attach -t agrobot
+		;;
 esac
--- setup.sh
+++ setup.sh
@@ -7,61 +7,61 @@
 # - Make sure you run this from the root of the top-level repo
 
 function printInfo {
-  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+  	echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+  	echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
 }
 
 function printError {
-  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+  	echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
 }
 
 if [ "$(uname -m)" == "aarch64" ]; then
 
-  printInfo "Setting up AgRobot on a Raspberry Pi 5"
-            
-  # Install Docker if not already installed
-  if ! [ -x "$(command -v docker)" ]; then
-      curl -fsSL https://get.docker.com -o get-docker.sh
-      sudo sh get-docker.sh
-      rm get-docker.sh
-      sudo usermod -aG docker agrobot
-  else
-      printWarning "Docker is already installed"
-  fi
+	printInfo "Setting up AgRobot on a Raspberry Pi 5"
+				
+	# Install Docker if not already installed
+	if ! [ -x "$(command -v docker)" ]; then
+		curl -fsSL https://get.docker.com -o get-docker.sh
+		sudo sh get-docker.sh
+		rm get-docker.sh
+		sudo usermod -aG docker agrobot
+	else
+		printWarning "Docker is already installed"
+	fi
 
-  # Install dependencies
-  sudo apt update
-  sudo apt upgrade -y
-  sudo apt install -y git vim mosh tmux
+	# Install dependencies
+	sudo apt update
+	sudo apt upgrade -y
+	sudo apt install -y git vim mosh tmux
 
-  # Set up volumes
-  mkdir bag
+	# Set up volumes
+	mkdir bag
 
-  # Set up udev rules
-  sudo cp config/udev/00-teensy.rules /etc/udev/rules.d/00-teensy.rules
-  sudo udevadm control --reload-rules
-  sudo udevadm trigger
+	# Set up udev rules
+	sudo cp config/udev/00-teensy.rules /etc/udev/rules.d/00-teensy.rules
+	sudo udevadm control --reload-rules
+	sudo udevadm trigger
 
-  # Copy repos from GitHub
-  git clone https://github.com/BYUAgrobotics/agrobot-ros2.git
-  git clone https://github.com/BYUAgrobotics/agrobot-teensy.git
+	# Copy repos from GitHub
+	git clone https://github.com/BYUAgrobotics/agrobot-ros2.git
+	git clone https://github.com/BYUAgrobotics/agrobot-teensy.git
 
 else
 
-  printInfo "Setting up AgRobot on a development machine"
+	printInfo "Setting up AgRobot on a development machine"
 
-  # Install dependencies
-  sudo apt update
-  sudo apt install -y git vim mosh tmux
+	# Install dependencies
+	sudo apt update
+	sudo apt install -y git vim mosh tmux
 
-  # Set up volumes
-  mkdir bag
+	# Set up volumes
+	mkdir bag
 
-  # Copy repos from GitHub
-  git clone https://github.com/BYUAgrobotics/agrobot-ros2.git
-  git clone https://github.com/BYUAgrobotics/agrobot-teensy.git
+	# Copy repos from GitHub
+	git clone https://github.com/BYUAgrobotics/agrobot-ros2.git
+	git clone https://github.com/BYUAgrobotics/agrobot-teensy.git
 
 fi
